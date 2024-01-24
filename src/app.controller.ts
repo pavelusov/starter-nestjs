4cc5b26ec369b2782de76557801855c83245718b
pavel.usov <ipavelusov@gmail.com>
2024-01-24 11:42:35 +0500

    Added /coin

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { interval, map, Observable } from 'rxjs';
+
+export interface MessageEvent {
+  data: string | object;
+  id?: string;
+  type?: string;
+  retry?: number;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +16,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('/coin')
+  coin(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map((value) => ({ data: { hello: `world ${value}` } })),
+    );
   }
 }
